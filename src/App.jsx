10fa315f7292b36d31828658c59b919245f37b74
Rasheduzzaman <rasheduzzaman1978@gmail.com>
2026-03-29 21:50:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+import Navbar from './components/Navbar/Navbar'
+import './App.css'
+import Banner from './components/Banner/Banner'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar></Navbar>
+      <Banner></Banner>
+     
+    </>
+  )
+}
+
+export default App
